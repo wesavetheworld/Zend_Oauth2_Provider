98bd31cc7edf3ff3066647ca68ad8e5a4902232c
Adar Porat <hadar@mybuilding.org>
2013-02-05 17:36:16 -0500

    fixing web_app recipe

--- recipes/cookbooks/web_app/recipes/default.rb
+++ recipes/cookbooks/web_app/recipes/default.rb
@@ -5,11 +5,13 @@ include_recipe "apache2"
 include_recipe "apache2::mod_ssl"
 include_recipe "apache2::mod_rewrite"
 
+# Install php 5.4
 execute "rpm --force -Uvh http://repo.webtatic.com/yum/el6/latest.rpm"
 execute "yum --enablerepo=webtatic -y install php54w-devel php54w-gd php54w-imap php54w-mbstring php54w-mysql php54w-pdo php54w-pecl-apc php54w-pecl-memcache php54w-snmp php54w-soap php54w-xml php54w-xmlrpc"
 
-command 'service iptables stop'  
-command 'chkconfig iptables off'  
+# Disable iptables for development box
+execute "service iptables stop"  
+execute "chkconfig iptables off"  
 
 
  web_app "web_app" do
